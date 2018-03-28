8a895fd341c382665f2adcec23b0b7a1bc0d7014
ppp31013 <37055290+ppp31013@users.noreply.github.com>
2018-03-28 23:23:34 +0800

    Update calculator.py

--- 0326/來源/calculator.py
+++ 0326/來源/calculator.py
@@ -1,1 +1,9 @@
-
+def  calculator（a，b，c）：
+    if（c == “ + ”）：
+        return（int（a）+ int（b））
+    if（c == “ - ”）：
+        return（int（a）- int（b））
+    if（c == “ * ”）：
+        return（int（a）* int（b））
+    if（c == “ / ”）：
+        返回（int（a）/ int（b））
